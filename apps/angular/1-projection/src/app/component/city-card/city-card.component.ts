3908cf8d048b8181c576344542f6df96cc6d68d9
Marcel Goldammer <marcel.goldammer@web.de>
2024-08-24 17:52:29 +0200

    feat(challenge-1): added new CityCardComponent

--- apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: `
+    <app-card [list]="cities()" class="bg-light-blue" (add)="addNewCity()">
+      <img src="assets/img/city.png" width="200px" />
+      <ng-template #itemTemplate let-city>
+        <app-list-item [name]="city.name" (delete)="deleteCity(city.id)" />
+      </ng-template>
+    </app-card>
+  `,
   standalone: true,
-  imports: [],
+  imports: [CardComponent, ListItemComponent],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  private http = inject(FakeHttpService);
+  private store = inject(CityStore);
 
-  ngOnInit(): void {}
+  cities = this.store.cities;
+
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((c) => this.store.addAll(c));
+  }
+
+  addNewCity() {
+    this.store.addOne(randomCity());
+  }
+
+  deleteCity(id: number) {
+    this.store.deleteOne(id);
+  }
 }
